8b02df22b99723e5018a80257dcd0e2d9faaaaed
Sarath S <sarathsuryasss48@gmail.com>
2024-03-11 01:34:47 +0530

    fourth

---
+++ maxHeap.js
@@ -0,0 +1,46 @@
+class MaxHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  insert(value) {
+    this.heap.push(value);
+   this.maxHeapify()
+  } 
+  maxHeapify(){
+    let child = this.heap.length-1
+    
+    while(child > 0) {
+      let parent = Math.floor(child/2)
+      if(this.heap[child] > this.heap[parent]){
+        let temp = this.heap[parent]
+        this.heap[parent] = this.heap[child]
+        this.heap[child] = temp
+        child = parent
+      }else{
+        return
+      }
+    }
+  }
+    delete(){
+       this.heap.shift()
+       this.heap.unshift(this.heap.pop())
+    
+  }
+
+  printHeap() {
+    console.log(this.heap);
+  }
+}
+
+// Example usage:
+const heap = new MaxHeap();
+heap.insert(10);
+heap.insert(15);
+heap.insert(30);
+heap.insert(40);
+heap.insert(9)
+heap.insert(41)
+heap.insert(100)
+
+heap.printHeap();
